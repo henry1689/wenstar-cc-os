ae4de9baf0727128bac64b2b00aa7bdf77061ef4
henry1689 <henry@example.com>
2026-08-06 19:11:01 +0800

    feat(tianquan-rpc): index 导出 Tribody40DResult/Collect40DParams 类型

--- src/tianquan-rpc/index.ts
+++ src/tianquan-rpc/index.ts
@@ -1,5 +1,5 @@
 export { TianquanRPCClient, createTianquanClient, TianquanRPCError, TianquanNotReadyError } from './TianquanRPCClient.js';
-export type { TianquanRPCConfig, HealthStatus, WorkflowResult, LintReport, ArchReport, SQLAuditReport, SnapshotResult, SpecResult, WorkflowListResult } from './TianquanRPCClient.js';
+export type { TianquanRPCConfig, HealthStatus, WorkflowResult, LintReport, ArchReport, SQLAuditReport, SnapshotResult, SpecResult, WorkflowListResult, Tribody40DResult, Collect40DParams } from './TianquanRPCClient.js';
 export { MasterHarris, getMasterHarris, initMasterHarris, classifyIntent, TaskDomain, RouteTag } from './MasterHarris.js';
 export type { MasterTask, DispatchResult, IntentClassification } from './MasterHarris.js';
 export { SpecLoader, getSpecLoader, loadDomainSpecs } from './spec_loader.js';
